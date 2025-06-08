1c0f5dc88aa30e373fb74374e2f7ff4dbe7e5fa5
Cory Moon <cory@moonlab.cloud>
2025-06-08 10:51:34 -0500

    add ability to pass component to footer

--- src/lib/Modal/LightboxFooter.svelte
+++ src/lib/Modal/LightboxFooter.svelte
@@ -1,49 +1,75 @@
 <script lang="ts">
-    import i18n from '$lib/i18n'
-    import type { GalleryState, ImagePreset, I18n } from '$lib/Types'
+    import i18n from "$lib/i18n";
+    import type { GalleryState, ImagePreset, I18n } from "$lib/Types";
 
-    export let imagePreset: ImagePreset
-    export let title = ''
-    export let description = ''
-    export let gallery: GalleryState | null = null
+    export let imagePreset: ImagePreset;
+    export let title = "";
+    export let description = "";
+    export let gallery: GalleryState | null = null;
+    export let component: typeof import("svelte").SvelteComponent | null = null;
 
-    const generateLocalizedGalleryCounter = (i18n: I18n, gallery: GalleryState | null) => {
+    const generateLocalizedGalleryCounter = (
+        i18n: I18n,
+        gallery: GalleryState | null,
+    ) => {
         if (gallery !== null) {
-            return i18n.generateLocalizedGalleryCounter(gallery.activeImage, gallery.imageCount)
+            return i18n.generateLocalizedGalleryCounter(
+                gallery.activeImage,
+                gallery.imageCount,
+            );
         }
-    }
+    };
 
-    $: localizedGalleryCounter = generateLocalizedGalleryCounter($i18n, gallery)
+    $: localizedGalleryCounter = generateLocalizedGalleryCounter(
+        $i18n,
+        gallery,
+    );
 </script>
 
-<div class="svelte-lightbox-footer" class:fullscreen={imagePreset === 'fullscreen'} {...$$restProps}>
-    <h2>
+{#if component}
+    <svelte:component
+        this={component}
         {title}
-    </h2>
-    <h5>
         {description}
-    </h5>
-    {#if gallery !== null}
-        <p>
-            {localizedGalleryCounter}
-        </p>
-    {/if}
-</div>
+        {gallery}
+        {imagePreset}
+        {localizedGalleryCounter}
+        {...$$restProps}
+    />
+{:else}
+    <div
+        class="svelte-lightbox-footer"
+        class:fullscreen={imagePreset === "fullscreen"}
+        {...$$restProps}
+    >
+        <h2>
+            {title}
+        </h2>
+        <h5>
+            {description}
+        </h5>
+        {#if gallery !== null}
+            <p>
+                {localizedGalleryCounter}
+            </p>
+        {/if}
+    </div>
 
-<style>
-    div.svelte-lightbox-footer {
-        width: 100%;
-        height: auto;
-        color: white;
-        text-align: left;
-        position: absolute;
-    }
-    div.svelte-lightbox-footer.fullscreen {
-        position: fixed;
-        z-index: 5;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        padding-left: 1rem;
-    }
-</style>
+    <style>
+        div.svelte-lightbox-footer {
+            width: 100%;
+            height: auto;
+            color: white;
+            text-align: left;
+            position: absolute;
+        }
+        div.svelte-lightbox-footer.fullscreen {
+            position: fixed;
+            z-index: 5;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            padding-left: 1rem;
+        }
+    </style>
+{/if}
